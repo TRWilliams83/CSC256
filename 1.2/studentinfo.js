a29d1f327c864b925810874bbcf7b8ed67a43cd3
TRWilliams83 <72673690+TRWilliams83@users.noreply.github.com>
2026-07-19 15:22:17 -0700

    Add student information display in studentinfo.js

---
+++ 1.2/studentinfo.js
@@ -0,0 +1,11 @@
+// Student information is stored in JavaScript instead of the HTML.
+const studentName = "Timothy R. Williams";
+const studentMajor = "Digital Maker and Fabrication";
+const studentEmail = "TWILLIAMS88046@uat.edu";
+const graduationDate = "August 23, 2026";
+
+// Display the student information in the HTML elements.
+document.getElementById("studentName").textContent = studentName;
+document.getElementById("studentMajor").textContent = studentMajor;
+document.getElementById("studentEmail").textContent = studentEmail;
+document.getElementById("graduationDate").textContent = graduationDate;
